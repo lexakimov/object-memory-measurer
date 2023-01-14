529da394e0e7c8c12d8b281aab38b5f1b4be8db8
Alexey Akimov <lex.akimov23@gmail.com>
2023-01-14 22:59:14 +0700

    add javadoc

--- src/test/java/com/github/lexakimov/omm/classes/ClassWithTwoIntFieldAndOneNonNullObject.java
+++ src/test/java/com/github/lexakimov/omm/classes/ClassWithTwoIntFieldAndOneNonNullObject.java
@@ -1,6 +1,8 @@
 package com.github.lexakimov.omm.classes;
 
 /**
+ * 24 + 16 = 40 bytes
+ *
  * @author akimov
  * created at: 08.01.2023 13:24
  */
@@ -15,6 +17,9 @@ public class ClassWithTwoIntFieldAndOneNonNullObject {
     int a = 100;
     Object d = new SomeObject();
 
+    /**
+     * 16 bytes
+     */
     public static class SomeObject {
         public static final String FQN =
                 "com.github.lexakimov.omm.classes.ClassWithTwoIntFieldAndOneNonNullObject$SomeObject";
